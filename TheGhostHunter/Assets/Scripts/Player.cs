3310ffcb85bae293dc99dfdfa22f749f6e6c25c7
Choiseungpyo <csp9511@naver.com>
2022-02-02 16:47:21 +0900

    전체 BG, HP
     - 전체 BG GameObject -> World Space Canvas의 UI로 바꿈
    
     - HP 리소스 변경
     - HP 관련 코드 GameObject.SetActive()로 이미지를 없애는 형식 -> 스프라이트만 변경하는 형식으로 바꿈.

--- TheGhostHunter/Assets/Scripts/Player.cs
+++ TheGhostHunter/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Player : MonoBehaviour
@@ -17,6 +18,11 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         instance = this;
+        for(int i=0; i<HpImg.Length; i++)
+        {
+            HpImg[i].SetActive(true);
+        }
+
         LoadHpData();
     }
 
@@ -32,28 +38,28 @@ public class Player : MonoBehaviour
         {
             if(hp == 3) //hp 3
             {
-                HpImg[i].SetActive(true);
+                HpImg[i].GetComponent<Image>().sprite = Resources.Load<Sprite>("Player/FullHp");
             }
             else if(hp == 2) //hp 2
             {
-                HpImg[i].SetActive(true);
+                HpImg[i].GetComponent<Image>().sprite = Resources.Load<Sprite>("Player/FullHp");
                 if (i == 2)
                 {
-                    HpImg[i].SetActive(false);
+                    HpImg[i].GetComponent<Image>().sprite = Resources.Load<Sprite>("Player/WornUpHP");
                     break;
                 }              
             }
             else if(hp == 1)
             {
-                HpImg[i].SetActive(true);
+                HpImg[i].GetComponent<Image>().sprite = Resources.Load<Sprite>("Player/FullHp");
                 if (i == 1 || i ==2)
                 {
-                    HpImg[i].SetActive(false);
+                    HpImg[i].GetComponent<Image>().sprite = Resources.Load<Sprite>("Player/WornUpHP");
                 }              
             }
             else
             {
-                HpImg[i].SetActive(false);
+                HpImg[i].GetComponent<Image>().sprite = Resources.Load<Sprite>("Player/WornUpHP");
             }
         }
     }
